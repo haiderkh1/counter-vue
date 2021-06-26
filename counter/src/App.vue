f6300a4c5922e488042404341c8aa04293576425
Haider <haideryouxd4@gmail.com>
2021-06-26 16:08:57 +0500

    birthday-counter

---
+++ counter/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    Hello world!
+  </div>
+  <Counter
+    :year="2022"
+    :month="0"
+    :date="4"
+    :hour="0"
+    :minute="0"
+    :second="0"
+    :millisecond="0"
+  />
+</template>
+<script>
+import Counter from "./components/Counter";
+export default {
+  components: {
+    Counter,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
